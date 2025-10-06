106674382ec0b5c20a2d0cdc1575e44b911b72f0
Parciwal <parciwal2@gmail.com>
2025-10-06 12:54:15 +0200

    further visitro login

--- TrappistOS/TrappistOS/Kernel.cs
+++ TrappistOS/TrappistOS/Kernel.cs
@@ -7,15 +7,17 @@ namespace TrappistOS
 {
     public class Kernel : Sys.Kernel
     {
+        UserLogin userInfo;
         protected override void BeforeRun()
         {
+            userInfo = new UserLogin();
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
-            UserLogin.currentUser = UserLogin.VisitorLogin();
+            userInfo.VisitorLogin();   
         }
 
         protected override void Run()
         {
-            Console.Write($"{UserLogin.currentUser.username}:Input: ");
+            Console.Write($"{userInfo.currentUser.username}:Input: ");
             var input = Console.ReadLine();
             Console.Write("Text typed: ");
             Console.WriteLine(input);
