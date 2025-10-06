8a40f425a7f7a41d104e773b392dcf677fc78650
Parciwal <parciwal2@gmail.com>
2025-10-06 15:52:46 +0200

    fix: add program memory

--- TrappistOS/TrappistOS/Kernel.cs
+++ TrappistOS/TrappistOS/Kernel.cs
@@ -7,7 +7,8 @@ namespace TrappistOS
 {
     public class Kernel : Sys.Kernel
     {
-
+        // each program needs one space here
+        ProgramClass[] ProgramMemory = new ProgramClass[6];
         protected override void BeforeRun()
         {
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
