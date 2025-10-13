b77e18c39f6662f012f33480f8b5391bd3d766b2
Herlan Ribeiro Rocha <herlan.ribeiro-rocha@capgemini.com>
2025-10-13 10:10:24 +0200

    first commit implemented FS

---
+++ TrappistOS/TrappistOS/FileSystemManager.cs
@@ -0,0 +1,198 @@
+﻿using Cosmos.System.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrappistOS
+{
+    internal class FileSystemManager
+    {
+        private CosmosVFS fs;
+        string currentDir = @"0:\";
+        Stack<string> dirHistory = new Stack<string>();
+
+        public void fsInitialize()
+        {
+            fs = new CosmosVFS();
+            Cosmos.System.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+        }
+
+        public void showFreeSpace()
+        {
+            var available_space = fs.GetAvailableFreeSpace(@"0:\");
+            Console.WriteLine("Available Free Space: " + available_space);
+        }
+
+        public void listFiles()
+        {
+
+            var dirs = Directory.GetDirectories(currentDir);
+            var files = Directory.GetFiles(currentDir);
+
+            try
+            {
+                foreach(var dir in dirs)
+                {
+                    var dirInfo = new DirectoryInfo(dir);
+                    Console.WriteLine(@"\" + dirInfo.Name + @"\");
+                }
+
+                foreach (var file in files)
+                {
+                    var fileInfo = new DirectoryInfo(file);
+                    Console.WriteLine(fileInfo.Name);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void createFile(string filename)
+        {
+            string path = currentDir + @"\" + filename;
+
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    File.WriteAllText(path, "");
+                    Console.WriteLine("File created: " + path);
+                }
+                else
+                {
+                    Console.WriteLine("File already exist: " + path);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error creating file: " + e.Message);
+            }
+        }
+
+        public void createDirectory(string dirName)
+        {
+            string path = currentDir + @"\" + dirName;
+
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                    Console.WriteLine("Directory created: " + path);
+                }
+                else
+                {
+                    Console.WriteLine("Directory already exist: " + path);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error creating directory: " + e.Message);
+            }
+        }
+        public void deleteFileOrDir(string name)    
+        {
+
+            string path = currentDir + @"\" + name;
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    Console.WriteLine("File " + path + " deleted");
+                    return;
+                }
+
+
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path);
+                    Console.WriteLine("Directory " + path + " deleted");
+                    return;
+                }
+
+                else 
+                {
+                    Console.WriteLine("Directory or file doesn't exist");
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void readFromFile(String filename)
+        {
+            string path = currentDir + @"\" + filename;
+
+            try
+            {
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void changeDirectory(string newDir)
+        {
+
+            if(newDir == "..")
+            {
+                if (dirHistory.Count > 0) 
+                {
+                    currentDir = dirHistory.Pop();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("You are on the home directory");
+                }
+            }
+
+            string path = Path.Combine(currentDir, newDir);
+
+            if (Directory.Exists(path))
+            {
+                dirHistory.Push(currentDir);
+                currentDir = path;
+            }
+            else
+            {
+                Console.WriteLine("Directory doesn't exist");
+            }
+
+        }
+        
+        public void moveFile(string filename, string dest)
+        {
+            string file = currentDir + @"\" + filename;
+            string newPath = @"0:\" + dest;
+
+            try
+            {
+                File.Copy(file, newPath);
+                File.Delete(file);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public string getCurrentDir()
+        {
+            return currentDir;
+        }
+
+    }
+}
--- TrappistOS/TrappistOS/Kernel.cs
+++ TrappistOS/TrappistOS/Kernel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Cosmos.System.Graphics;
+using Cosmos.System.ScanMaps;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Sys = Cosmos.System;
 
@@ -7,18 +10,98 @@ namespace TrappistOS
 {
     public class Kernel : Sys.Kernel
     {
+        FileSystemManager fsManager;
 
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            fsManager = new FileSystemManager();
+            fsManager.fsInitialize();
+            Sys.KeyboardManager.SetKeyLayout(new DE_Standard());
+            Console.WriteLine("TrappistOS booted up!");
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
+
+            Console.Write(fsManager.getCurrentDir() + "> ");
             var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+
+            string[] args = input.Split(' ');
+
+            switch (args[0])
+            {
+                case "freespace":
+                    {
+                        fsManager.showFreeSpace();
+                        break;
+                    }
+                case "touch":
+                    {
+                        if (args.Length > 1)
+                        {
+                            fsManager.createFile(args[1]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Use: createfile <file_name>");
+                        }
+                        break;
+                    }
+                case "mkdir":
+                    {
+                        if (args.Length > 1)
+                        {
+                            fsManager.createDirectory(args[1]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Use: createdirectory <file_name>");
+                        }
+                        break;
+                    }
+
+                case "ls":
+                    {
+                        fsManager.listFiles();
+                        break;
+                    }
+
+                case "mv":
+                    {
+                        fsManager.moveFile(args[1], args[2]);
+                        break;
+                    }
+
+                case "cat":
+                    {
+                        fsManager.readFromFile(args[1]);
+                        break;
+                    }
+
+                case "rm":
+                    {
+                        fsManager.deleteFileOrDir(args[1]);
+                        break;
+                    }
+                case "cd":
+                    {
+                        fsManager.changeDirectory(args[1]);
+                        break;
+                    }
+                case "clear":
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Not a valid command");
+                        break;
+                    }
+
+            }
         }
     }
+
+
 }
