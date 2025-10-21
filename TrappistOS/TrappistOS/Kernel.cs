9dbff4baced11845164394ae4be37d5cd3411026
Fatih <semizfatih@outlook.com>
2025-10-21 10:42:24 +0200

    21.10 3

--- TrappistOS/TrappistOS/Kernel.cs
+++ TrappistOS/TrappistOS/Kernel.cs
@@ -12,23 +12,25 @@ namespace TrappistOS
 {
     public class Kernel : Sys.Kernel
     {
-        private static Sys.FileSystem.CosmosVFS FS;
-        public static string file;
+        // each program needs one space here
+        ProgramClass[] ProgramMemory = new ProgramClass[6];
+            private static Sys.FileSystem.CosmosVFS FS;
+            public static string file;
 
-        protected override void BeforeRun() {
-            FS = new Sys.FileSystem.CosmosVFS(); Sys.FileSystem.VFS.VFSManager.RegisterVFS(FS); FS.Initialize(true);
-            Console.Clear();
-            Console.WriteLine("TrappistOS booted successfully. Welcome.");
-            Sys.KeyboardManager.SetKeyLayout(new DE_Standard());
-        }
+            protected override void BeforeRun() {
+                FS = new Sys.FileSystem.CosmosVFS(); Sys.FileSystem.VFS.VFSManager.RegisterVFS(FS); FS.Initialize(true);
+                Console.Clear();
+                Console.WriteLine("TrappistOS booted successfully. Welcome. Type \"miv\" to access the text editor.");
+                Sys.KeyboardManager.SetKeyLayout(new DE_Standard());
+            }
 
-        protected override void Run() {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
+            protected override void Run() {
+                Console.Write("Input: ");
+                var input = Console.ReadLine();
 
-            if (input == "miv") {
-                MIV.MIV.StartMIV();
+                if (input == "miv") {
+                    MIV.MIV.StartMIV();
+                }
             }
         }
     }
-}
